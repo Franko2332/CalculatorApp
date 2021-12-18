2084651ea4656b3ed9f3f5f9050c7dae256ef220
Franko777 <khubul.george@inbox.ru>
2021-12-18 16:50:34 +0300

    init

--- app/src/main/java/ru/gb/calculatorapp/MainActivity.java
+++ app/src/main/java/ru/gb/calculatorapp/MainActivity.java
@@ -2,13 +2,29 @@ package ru.gb.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import ru.gb.calculatorapp.listeners.MyOnclickForCommaButton;
+import ru.gb.calculatorapp.listeners.MyOnclickListenerForClearButton;
+import ru.gb.calculatorapp.listeners.MyOnclickListenerForDeleteDigitButton;
+import ru.gb.calculatorapp.listeners.MyOnClickListenerForNumbButtons;
+import ru.gb.calculatorapp.listeners.MyOnclickListenerForOperandButtons;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private EditText editText;
+//    private MyCalculator myCalculator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_with_background_pic);
+        setContentView(R.layout.activity_main);
+        //init();
     }
+
+
 }
